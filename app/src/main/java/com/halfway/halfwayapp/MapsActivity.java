2b01aed97069fa2579953113138623be57bf311d
timferaco <timothy.feraco@mymail.champlain.edu>
2019-11-20 01:02:24 -0500

    Added button to App Bar
    Logic should go in onOptionsItemSelected

--- app/src/main/java/com/halfway/halfwayapp/MapsActivity.java
+++ app/src/main/java/com/halfway/halfwayapp/MapsActivity.java
@@ -1,47 +1,70 @@
 package com.halfway.halfwayapp;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class  MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+        private static final String TAG = MapsActivity.class.getSimpleName();
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Retrieve the content view that renders the map.
+            setContentView(R.layout.activity_maps);
+
+            // Get the SupportMapFragment and register for the callback
+            // when the map is ready for use.
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
+        /**
+         * Manipulates the map when it's available.
+         * The API invokes this callback when the map is ready for use.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+
+
+            // Position the map's camera near Sydney, Australia.
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+        }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        switch(item.getItemId()) {
+            case R.id.test_button:
+                //your action
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
