8a28f76841f5592ec56cf4e5699b6fd622654407
Paul Igo <35532313+paulIgo@users.noreply.github.com>
2020-02-02 17:20:28 -0500

    PullPlaceIDS

--- app/src/main/java/com/halfway/halfwayapp/MainActivity.java
+++ app/src/main/java/com/halfway/halfwayapp/MainActivity.java
@@ -4,19 +4,42 @@ import androidx.annotation.*;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import androidx.appcompat.app.*;
+
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private BottomSheetBehavior mBottomSheetBehavior;
     private TextView mTextViewState;
 
+    private ArrayList<String> mPlaceIDs;
+    private OkHttpClient mHTTPClient;
+    final private String MAPS_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?&location=";
+
+    final private String RESPONSE_TAG = "com.halfway.response";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlaceIDs = new ArrayList<String>();
+        mHTTPClient = new OkHttpClient();
 
         View bottomSheet = findViewById(R.id.bottom_sheet);
 
@@ -68,5 +91,55 @@ public class MainActivity extends AppCompatActivity {
                 mTextViewState.setText("Sliding...");
             }
         });
+        refresh();
     }
+
+    private class RefreshTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            Request request = new Request.Builder()
+                    .url("https://maps.googleapis.com/maps/api/place/textsearch/json?type=restuarant&location=44.482618,-73.209405&radius=10000&key=AIzaSyACLyHMHhi7tsD7JRHAD4zubgFVZ7TepQQ")
+                    .build();
+
+
+            try (Response response = mHTTPClient.newCall(request).execute()) {
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Error: Could not complete request.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(RESPONSE_TAG, result);
+            parseJSON(result);
+
+        }
+
+
+    }
+
+    private void refresh() {
+        RefreshTask rt = new RefreshTask();
+        rt.execute();
+    }
+
+    private void parseJSON(String jsonString) {
+        //mPhotosURLs.clear();
+        try {
+            JSONObject responseObject = new JSONObject(jsonString);
+            JSONArray items = responseObject.getJSONArray("results");
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject item = items.getJSONObject(i);
+                String urlString = item.getString("id");
+                mPlaceIDs.add(0, urlString);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("BLAH",Integer.toString(mPlaceIDs.size()));
+    }
+
 }
