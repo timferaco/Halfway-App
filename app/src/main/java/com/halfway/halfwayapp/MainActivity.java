c19b2a32da43ad52920b70fbaa97c14bc943a517
Tim Feraco <35465500+timferaco@users.noreply.github.com>
2020-02-12 02:03:50 -0500

    Added pins and polylines to map
    Also shrunk the sheet to see more map

--- app/src/main/java/com/halfway/halfwayapp/MainActivity.java
+++ app/src/main/java/com/halfway/halfwayapp/MainActivity.java
@@ -2,10 +2,28 @@ package com.halfway.halfwayapp;
 
 import androidx.annotation.*;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.halfway.halfwayapp.MapRequestHelpers.FetchURL;
+import com.halfway.halfwayapp.MapRequestHelpers.TaskLoadedCallback;
 import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.*;
@@ -13,10 +31,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -32,7 +54,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
     private BottomSheetBehavior mBottomSheetBehavior;
     private TextView mTextViewState;
 
@@ -41,8 +63,13 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<PlaceCard> mPlaces;
     private OkHttpClient mHTTPClient;
-    final private String MAPS_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?&location=";
 
+    private FirebaseFirestore db;
+    private GoogleMap mMap;
+    private Polyline drawDir;
+    private ArrayList<LatLng> latlngs;
+
+    final private String MAPS_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?&location=";
     final private String RESPONSE_TAG = "com.halfway.response";
 
 
@@ -50,6 +77,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        latlngs = new ArrayList<>();
+
+        // Get the SupportMapFragment and register for the callback
+        // when the map is ready for use.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         mPlaceAdapter = new PlaceAdapter();
         mPlaceRecycler = (RecyclerView) findViewById(R.id.places_recycler);
@@ -90,7 +126,114 @@ public class MainActivity extends AppCompatActivity {
                 //mTextViewState.setText("Sliding...");
             }
         });
+        fetchUserLocations();
         refresh();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready for use.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        fetchUserLocations();
+        switch(item.getItemId()) {
+            case R.id.test_button:
+
+                //Create marker
+                Marker m1 = mMap.addMarker(new MarkerOptions()
+                        .position(latlngs.get(0))
+                        .anchor(0.5f, 0.5f)
+                        .title("Position 1")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                Marker m2 = mMap.addMarker(new MarkerOptions()
+                        .position(latlngs.get(1))
+                        .anchor(0.5f, 0.5f)
+                        .title("Position 2")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latlngs.get(0)));
+
+                // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+                //https://developers.google.com/maps/documentation/android-sdk/views
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latlngs.get(0))      // Sets the center of the map to Mountain View
+                        .zoom(13)                   // Sets the zoom
+                        .bearing(0)                // Sets the orientation of the camera to east
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                //Create the URL to get request from first marker to second marker
+                String url = getRequestUrl(latlngs.get(0), latlngs.get(1));
+                FetchURL furl = new FetchURL(MainActivity.this);
+                furl.execute(url, "driving");
+
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    //Preparing for Directions API, returns the correct info
+    private String getRequestUrl(LatLng origin, LatLng dest) {
+        //Value of origin
+        String org = "origin=" + origin.latitude +","+origin.longitude;
+        //Value of destination
+        String destination = "destination=" + dest.latitude+","+dest.longitude;
+        //Set value enable the sensor
+        String sensor = "sensor=false";
+        //Mode for find direction
+        //!!! CAN CHANGE MODE !!!
+        String mode = "mode=driving";
+        //Build the full param
+        String param = org +"&" + destination + "&" +sensor+"&" +mode;
+        //Output format
+        String output = "json";
+        //Create url to request
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + param + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+
+    private void fetchUserLocations(){
+        db.collection("UserLocation")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                GeoPoint tempGP = (GeoPoint) document.get("latlng");
+                                LatLng tempLL = new LatLng(tempGP.getLatitude(), tempGP.getLongitude());
+
+                                latlngs.add(tempLL);
+
+
+                            }
+                        } else {
+                            Log.w("ACCESS_ERROR", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
 
     }
 
@@ -224,6 +367,13 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return mPlaces.size();
         }
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (drawDir != null)
+            drawDir.remove();
+        drawDir = mMap.addPolyline((PolylineOptions) values[0]);
     }
 
 }
